cc5004e2efc75e5b2600c890bfc59c5431b9277e
Tiku Brenda <brendagaelle6@gmail.com>
2020-06-17 05:00:40 +0100

    parent dashboard php file

---
+++ parent/ParentDashboard/PD.php
@@ -0,0 +1,167 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Parent Dashboard</title>
+	<link rel="stylesheet" type="text/css" href="bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="PD.css">
+
+	
+</head>
+<body class="bg-primar"> 
+
+
+<!--start row 1-->	<div class="row" id="main" style="background-image: url('bg1.jpg');">
+
+		<div class="col-sm-3">							
+		</div>
+
+		<div class="col-lg-6">							
+	<center>	
+	<h1 id="headline"><label id="L">L</label>EARNING <label id="G">G</label>AME</h1><br>
+	<h4 id="subheadline">Educate your kids all from your device</h4> <br>
+	<button type="button" class="btn" id="start">ADD CHILD ACCOUNT</button> <button type="button" class="btn" id="start1">VIEW KID'S PROFILE</button>
+	</center>
+		</div>
+
+		<div class="col-sm-3">							
+		</div>
+
+	</div>
+<!--end row 1-->
+
+<br>
+
+<!--start row 2desk-->
+<div class="row" id="row2desk" style="background-color:blue; margin-top: -23px; padding-top: 50px; padding-bottom: 50px;">
+
+		<div class="col-lg-4">
+		<center>	 <img class="img-responsive" src="Welcome.jpg" id="firstImages"> </center>
+		<center>	<h5><b>GET STARTED</b></h5>
+			<p>Add an account for your child(ren) and<br>get right into it.</p>
+			<button type="button" class="btn" id="btnn">ADD CHILD</button>	</center>
+		</div>
+
+		<div class="col-lg-4">
+		<center>	<img class="img-responsive" src="Lessons.jpg" id="firstImages">
+			<h5><b>LESSONS</b></h5>
+			<p>View our prepared lessons and<br>use at will.</p>
+			<button type="button" class="btn" id="btnn">LESSONS</button> </center>
+		</div>
+
+		<div class="col-lg-4">
+		<center>	<img class="idmg-responsive" src="Quizzes.jpg" id="firstImages">
+			<h5><b>QUIZZES</b></h5>
+			<p>We have a variety of preset quizzes<br>at your disposal.</p>
+			<button type="button" class="btn" id="btnn">QUIZZES</button> </center>
+		</div>
+</div>
+<!--end row 2desk-->	
+
+
+<!--start row 2mob-->	
+<div class="row" id="row2mob">
+
+		<div class="col-lg-4">
+		<center> <img class="img-responsive" src="Welcome.jpg" style="width: 500px;"> 
+		<br>
+			<h5><b>GET STARTED</b></h5>
+			<p>Add an account for your child(ren) and<br>get right into it.</p>
+			<button type="button" class="btn" id="btnnd">ADD CHILD</button>	</center>
+		</div>
+
+		<div class="col-lg-4" style="margin-top: 100px;">
+		<center> <img class="img-responsive" src="Lessons.jpg" style="width: 500px;"> 
+		<br>
+		<h5> <b>LESSONS</b> </h5>
+			<p>View our prepared lessons and<br>use at will.</p>
+			<button type="button" class="btn" id="btnnd">LESSONS</button> </center>
+		</div><br>
+
+		<div class="col-lg-4" style="margin-top: 100px;">
+		<center> <img class="img-responsive" src="Quizzes.jpg" style="width: 500px;">
+		<br>
+		<h5><b>QUIZZES</b></h5>
+			<p>We have a variety of preset quizzes<br>at your disposal.</p>
+			<button type="button" class="btn" id="btnnd">QUIZZES</button> </center>
+		</div>
+		
+</div>
+<!--end row 2mob-->	
+
+
+<br>
+
+<!--start row 3-->
+<div class="row" style="padding: 40px;">
+	<div class="col-sm-4">		
+		</div>
+	
+	<div class="col-sm-4">
+<center>	<button type="button" class="btn" id="desk" style="font-size: 30px; margin-bottom: 18px; color: black; background-color: pink;">VIEW CHILD RECORDS</button>
+		</center>
+		</div>
+	
+	<div class="col-sm-4">		
+		</div>
+	
+</div>
+<!--end row 3-->
+
+<!--start row 4-->
+<div class="row" id="dyk" style="background-color: blue; margin-top: -15px; padding: 50px;">
+
+		<div class="col-sm-1">		
+		</div>
+
+		<div class="col-md-7">
+		<h1 style="color: #45b6fe;">Did You Know?</h1><br>
+		<h5 style="color: white;">You can have as many child accounts linked to your account<br>as you wish.
+		So why the wait? Educate your kids NOW!</h5>							
+		</div>
+
+		<div class="col-sm-4" style="margin-top: 20px;"><br><br><br>
+<button type="button" class="btn" id="btnn">ADD A CHILD</button> <button type="button" id="btnn" class="btn">VIEW ALL KIDS</button>
+		</div>
+ </div> 
+<!--end row 4-->
+
+<!--start row 5-->
+ <div class="row" id="dyk" style="background-image: url('dots.jpg'); padding: 100px;">
+ 	<div class="col-sm-12">
+ 		<center><p style="font-size: 50px;"><b>DOWNLOAD THE APP</b></p></center> 
+	<center>	<img class="img-responsive" src="Gplay1.jpg" style="width: 200px;"> <img class="img-responsive" src="Gplay1.jpg" style="width: 200px;
+	"> </center>
+ 	</div>
+ </div> 
+<!--end row 5-->
+
+<!--start row 6-->
+<div class="row" id="row8mob" style="background-color: ">
+
+
+ <div class="col-lg-12" style="padding: 5px;">
+ 	<center><br>
+ 	<button type="button" class="btn" style="font-size: 70px; color: black; background-color: pink; margin-top: -400px;">VIEW CHILD RECORDS</button></center>
+</div>
+
+</div>
+<!--end row 6-->
+
+<br>
+
+<div id="mob1" style="background-color: blue;">
+<p> 
+<center style="font-size: 90px; color: #45b6fe;">Did You Know?</u></center><br>
+<center> <h5 style="font-size: 45px; color: white;">You can have as many child accounts linked to your account as you wish.
+So why the wait? Educate your kids NOW!</h5><br>
+<button type="button" class="btn" id="btnnds">ADD CHILD</button> <button type="button" class="btn" id="btnnds">VIEW ALL KIDS</button>
+</center>
+</p>
+
+<div id="mob2">
+<center><p style="font-size: 90px;"><b>DOWNLOAD THE APP</b></p></center> 
+	<center>	<img class="img-responsive" src="Gplay1.jpg" style="width: 300px;"> <img class="img-responsive" src="Gplay1.jpg" style="width: 300px;
+	"> </center>
+</div>
+</body>
+</html>
